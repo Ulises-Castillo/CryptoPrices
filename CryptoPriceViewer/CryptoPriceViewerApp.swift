c2e8ff685a7907878a99d5baa40db6d85cc71165
Ulysses Castillo <ulysses@catalina.io>
2021-09-18 19:30:03 -0700

    Initial Commit

---
+++ CryptoPriceViewer/CryptoPriceViewerApp.swift
@@ -0,0 +1,17 @@
+//
+//  CryptoPriceViewerApp.swift
+//  CryptoPriceViewer
+//
+//  Created by Ulysses Castillo on 9/18/21.
+//
+
+import SwiftUI
+
+@main
+struct CryptoPriceViewerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
